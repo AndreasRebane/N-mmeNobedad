d3105fee41afbdd44e78f0d43e6315256122f491
AndreasRebane <160438454+AndreasRebane@users.noreply.github.com>
2024-04-05 14:30:30 +0300

    I add butons

--- finals_template/createWindow.py
+++ finals_template/createWindow.py
@@ -3,6 +3,37 @@ from tkinter import *
 windowHeight = 500
 windowWidth = 500
 
+
+
+class button:
+    list = []
+
+    def on_press(self, event):
+        print("aghhhhhhh")
+        self.pressed = True
+
+    def on_release(self, event):
+        print("aghhhhhhh")
+        self.pressed = False
+
+    def __init__(self, x, y, w, h, color):
+        self.x = x
+        self.y = y
+        self.button = Button(window, height=h, width=w, bg=color, image=photoimage)
+        self.button.bind("<ButtonPress>", self.on_press)
+        self.button.bind("<ButtonRelease>", self.on_release)
+        self.pressed = False
+
+
+    
+        button.list.append(self)
+
+    def place(self):
+        self.pressed = False
+        self.button.place(x=self.x, y=self.y)
+
+
+
 # Initializing tkinter window-------------------------------------
 
 window=Tk()
@@ -11,7 +42,8 @@ window.geometry(f"{windowWidth}x{windowHeight}")
 canvas = Canvas(window, width=windowWidth, height=windowHeight, bg="white")
 canvas.grid()
 
-
+photo = PhotoImage("amogus.png") 
+photoimage = photo.subsample(3, 3) 
 
 def draw_rectangle(x, y, width, height, color): # X, Y --> UPPER-LEFT corner of the rectangle
     canvas.create_rectangle(x, y, (x+width), (y+height), fill=color, outline="black")
@@ -23,4 +55,8 @@ def draw_circle(x, y, radius, color): # X, Y --> CENTER of the circle
 
 def draw_text(x, y, t, fontSize): # X, Y --> CENTER coordinates   t --> your text
     canvas.create_text(x, y, text=t, fill="black", font=('Helvetica %s bold', fontSize))
+
+def updateWindow():
+    window.update()
+
 
--- finals_template/main.py
+++ finals_template/main.py
@@ -2,8 +2,7 @@ from time import sleep
 from math import floor, ceil # need on ümardamiseks kasulikud
 from random import randint, choice # choice on väga kasulik, kui on vaja valida suvaline element listist
 from ReadOrWrite import readFromFile, writeToFile # readFromFile(line_number) / writeToFile(text, line_number)
-from createWindow import draw_circle, draw_rectangle, draw_text, window, canvas
-
+from createWindow import draw_circle, draw_rectangle, draw_text, window, canvas, button, updateWindow
 
 
 stopped = False # the ending condition for the whole program
@@ -13,6 +12,7 @@ stopped = False # the ending condition for the whole program
 #Added this so closing the window doesn't crash the program
 window.protocol("WM_DELETE_WINDOW", lambda: globals().update({'stopped': True})) 
 
+b = button(0, 0, 20, 10, "red")
 
 while (stopped == False): # stopped is just an arbitrary ending condition
     canvas.delete("all")
@@ -25,6 +25,12 @@ while (stopped == False): # stopped is just an arbitrary ending condition
     draw_circle(120, 150, 20, "red")
     draw_text(120, 180, "Näide", 20)
 
+    b.place()
+
+    if(b.pressed == True):
+        print("pressed")
 
 
-    window.update()
+    
+ 
+    updateWindow()
