0c07fc8d339049ab98be14cbcac66d1899d6d829
Markus <markus.leppoja06@gmail.com>
2024-04-06 14:14:23 +0300

    d

--- map/pathfinding.py
+++ map/pathfinding.py
@@ -41,7 +41,7 @@ def findPathCoords():
         #print(endPos)
         for coord in coordinates:
             length = sqrt((coord[0] - startPos[0])**2 + (coord[1] - startPos[1])**2)
-            if length < 300: #this dumb, me fix later (possible no coords fount -> big bad)
+            if length < 15: #this dumb, me fix later (possible no coords fount -> big bad)
                 pathCoords.append(coord)
 
         for pathCoord in pathCoords:
