a718a5ab2e0bd0a33f2a3212e76c4772730036d6
Markus <markus.leppoja06@gmail.com>
2024-04-06 10:49:54 +0300

    modified:   map/search.py

--- map/search.py
+++ map/search.py
@@ -1,20 +1,16 @@
-class totalBuildings:
-    count = 0
-
-class placeInfo:
-    def __init__(self, __xCordinate, __yCordinate, __nameSynonys, __officialName) -> None:
-        self.xCordinate = __xCordinate
-        self.yCordinate = __yCordinate
-        self.nameSynonyms = __nameSynonys #list
-        self.officialName = __officialName
-        pass
-    id = totalBuildings.count
-    totalBuildings.count = totalBuildings.count + 1
-    officialName = "NAMEHERE"
-    xCordinate = 0
-    yCordinate = 0
-    nameSynonyms = {"NONE"}
+from coordinates import *
 
 
+def findClosestMatch(searchString: str):
+    closestMachIDList 
+    
+    
+    for x in LISTNAMEHERE:
+        if (searchString.lower().find(x.officialName.lower())):
+            closestMachIDList.append(x.id)
+            
 
-def findClosestMatch(searchString):
+
+        for subNames in x.
+        if (searchString.lower().find())
+    
