ac91550565fecafb10e50dd7d096328409445c73
Markus <markus.leppoja06@gmail.com>
2024-04-06 10:31:23 +0300

    name

---
+++ map/coordinates.py
@@ -0,0 +1,11 @@
+class placeInfo:
+    def __init__(self, __xCordinate, __yCordinate, __nameSynonys, __officialName) -> None:
+        self.xCordinate = __xCordinate
+        self.yCordinate = __yCordinate
+        self.nameSynonyms = __nameSynonys #list
+        self.officialName = __officialName
+        pass
+    officialName = "NAMEHERE"
+    xCordinate = 0
+    yCordinate = 0
+    nameSynonyms = {}
