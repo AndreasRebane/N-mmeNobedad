f22b00005ccf2db7e7a56371bf350ef951b73171
Markus <markus.leppoja06@gmail.com>
2024-04-06 10:35:18 +0300

    modified:   map/coordinates.py

--- map/coordinates.py
+++ map/coordinates.py
@@ -1,3 +1,6 @@
+class totalBuildings:
+    count = 0
+
 class placeInfo:
     def __init__(self, __xCordinate, __yCordinate, __nameSynonys, __officialName) -> None:
         self.xCordinate = __xCordinate
@@ -5,7 +8,9 @@ class placeInfo:
         self.nameSynonyms = __nameSynonys #list
         self.officialName = __officialName
         pass
+    id = totalBuildings.count
+    totalBuildings.count = totalBuildings.count + 1
     officialName = "NAMEHERE"
     xCordinate = 0
     yCordinate = 0
-    nameSynonyms = {}
+    nameSynonyms = {"NONE"}
