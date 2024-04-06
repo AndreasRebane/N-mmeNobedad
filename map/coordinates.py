54202d322f4de5c429bd4a64f98514b43a5b2622
Uku Reitel <uku.reitel@gmail.com>
2024-04-06 10:35:32 +0300

    Made place object more compete

--- map/coordinates.py
+++ map/coordinates.py
@@ -1,11 +1,13 @@
 class placeInfo:
-    def __init__(self, __xCordinate, __yCordinate, __nameSynonys, __officialName) -> None:
-        self.xCordinate = __xCordinate
-        self.yCordinate = __yCordinate
-        self.nameSynonyms = __nameSynonys #list
-        self.officialName = __officialName
-        pass
     officialName = "NAMEHERE"
     xCordinate = 0
     yCordinate = 0
     nameSynonyms = {}
+    subPlaces = []
+    
+    def __init__(self, __xCordinate: int, __yCordinate: int, __nameSynonys: list, __officialName: str, __subPlaces: list) -> None:
+        self.xCordinate = __xCordinate
+        self.yCordinate = __yCordinate
+        self.nameSynonyms = __nameSynonys #list
+        self.officialName = __officialName
+        self.subPlaces = __subPlaces
