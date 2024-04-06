7a2d7a6d937d1c676cfedf2c99dcf97d39afdad4
Markus <markus.leppoja06@gmail.com>
2024-04-06 13:06:56 +0300

    modified:   map/__pycache__/search.cpython-312.pyc 	modified:   map/locationSearch.py

--- map/locationSearch.py
+++ map/locationSearch.py
@@ -1,28 +1,60 @@
 from search import *
 
 class locationParameters:
+    def __init__(self, __startX: int, __startY: int, __endX: int, __endY: int) -> None:
+        self.xStartPos = __startX
+        self.yStartPos = __startY
+        self.xEndPos = __endX
+        self.yEndPos = __endY
+    pass
     xStartPos = 0
     yStartPos = 0
-    targetBuildingID = 0
+    xEndPos = 0
+    yEndPos = 0
+
 
 def searchForLocation():
-    inputLocation = str(input("Enter Location:"))
-    result: list = findClosestMatch(inputLocation)
+    global userInputData
+    inputLocation = str(input("Sisesta lõppkoht:"))
+    resultID1 = peidetudLeiaHooneID(findClosestMatch(inputLocation))
 
-    if (len(result) != 0):
-        print("\n")
-        print("Kirjuta sobiva hoone number ID: ")
-        print("ID: Hoone nimi")
-        for x in result:
-            print(str(x) + " : " + str(buildingList[x].officialName))
-        
-        print("\n" * 2)
-        inputID = int(input("ID:"))
-        
-
-    else:
-        print("\n" * 15)
-        print("Couldn't find any location with your input data. Input data: " + str(inputLocation))
+    if (resultID1 == -1):
         return
+
+    buildInstance1 = getBuildingInstance(resultID1)
+
+    startingLocationString = str(input("Sisesta algushoone:"))
+    resultID2 = peidetudLeiaHooneID(findClosestMatch(startingLocationString))
+
+    if (resultID2 == -1):
+        return
+    
+    buildInstance2 = getBuildingInstance(resultID2)
+
+    global userInputData
+    userInputData = locationParameters(buildInstance2.xCordinate, buildInstance2.yCordinate, buildInstance1.xCordinate, buildInstance1.yCordinate)
+
+    print(userInputData.xStartPos)
+    print(userInputData.yStartPos)
+    print(userInputData.xEndPos)
+    print(userInputData.yEndPos)
+        
+def peidetudLeiaHooneID(result: list):
+    if (len(result) == 0):
+        print("\n" * 15)
+        print("Couldn't find any location with your input data.")
+        return -1
+    
+    print("\n")
+    print("Kirjuta sobiva hoone number ID: ")
+    print("ID: Hoone nimi")
+    for x in result:
+        print(str(x) + " : " + str(buildingList[x].officialName))
+        
+    print("\n" * 2)
+    inputID = int(input("ID:"))
+    return inputID
+
+
 
 searchForLocation()
